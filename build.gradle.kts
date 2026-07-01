8d04981de7914f0cb319ab764683a6a656dec768
vgdgf <132031546+vgdgf@users.noreply.github.com>
2026-07-01 18:24:38 +0200

    Update build.gradle.kts for plugin and dependency management
    Refactor build.gradle.kts to use plugin management and dependency resolution management.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,17 @@
-plugins {
-    id("com.android.application") version "8.5.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "اسم_مشروعك"
+include(":app")
