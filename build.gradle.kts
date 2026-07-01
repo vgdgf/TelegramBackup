a5ce39edc6889a67b6d137c430255eeba8b28df9
vgdgf <132031546+vgdgf@users.noreply.github.com>
2026-07-01 18:30:48 +0200

    Refactor build.gradle.kts to simplify plugin management
    Removed plugin and dependency resolution management sections.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,4 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
+plugins {
+    id("com.android.application") version "8.5.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "TelegramBackup"
-include(":app")
